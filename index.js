9e27ac1e3c6ba2421a06497e4562af5ad7b6135b
Seyamkk <90871787+Seyamkk@users.noreply.github.com>
2026-03-03 20:02:01 +0000

    index eklendi

---
+++ index.js
@@ -0,0 +1,58 @@
+const axios = require("axios");
+
+const TOKEN = "8720086137";
+const CHAT_ID = "AAEjaw3Gl3qmDEARIiX79CluHyieeTWO7xw";
+
+const PRODUCT_URL = "https://porima3d.com/products/porima-eco-smart-pla-filament.js";
+
+const COLORS = [
+  "Beyaz",
+  "Sarı",
+  "Kahverengi",
+  "Turkuaz",
+  "Mavi",
+  "Gri",
+  "Siyah"
+];
+
+let lastStatus = {};
+
+async function checkStock() {
+  try {
+    const response = await axios.get(PRODUCT_URL);
+    const data = response.data;
+
+    let message = "🔔 PORIMA PLA STOK\n\n";
+    let changed = false;
+
+    COLORS.forEach(color => {
+      const variant = data.variants.find(v =>
+        v.option1 === color && v.option2 === "1.75mm"
+      );
+
+      if (variant) {
+        const inStock = variant.available;
+        const status = inStock ? "🟢 Stokta" : "🔴 Tükendi";
+
+        message += `${status} - ${color}\n`;
+
+        if (lastStatus[color] !== inStock) {
+          changed = true;
+          lastStatus[color] = inStock;
+        }
+      }
+    });
+
+    if (changed) {
+      await axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+        chat_id: CHAT_ID,
+        text: message
+      });
+    }
+
+  } catch (err) {
+    console.log("Hata:", err.message);
+  }
+}
+
+checkStock();
