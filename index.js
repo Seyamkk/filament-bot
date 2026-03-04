0f378c94dde441284948eafd443d9a02159d2963
Seyamkk <90871787+Seyamkk@users.noreply.github.com>
2026-03-04 03:49:27 +0300

    Update index.js
    index eklendi

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const TOKEN = "AAEjaw3Gl3qmDEARIiX79CluHyieeTWO7xw";
-const CHAT_ID = "8720086137";
+const CHAT_ID = "1050200289";
 
 const PRODUCT_URL = "https://porima3d.com/products/porima-eco-smart-pla-filament.js";
 
